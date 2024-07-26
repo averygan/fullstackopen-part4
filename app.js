5711b992ea829cccdcab494ff0e4f05359876eed
averygan <averygan19@gmail.com>
2024-07-27 00:24:12 +0800

    completed ex 4.15 added user schema, post and get methods for users

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
@@ -20,5 +21,6 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
